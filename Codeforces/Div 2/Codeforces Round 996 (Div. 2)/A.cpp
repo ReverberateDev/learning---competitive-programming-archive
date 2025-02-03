505375cef36d6a00f33319ea7605871d3eb674d9
Raphael Lim Fu En <raphaellim.dev@gmail.com>
2025-02-03 07:35:43 +0000

    added A.cpp

---
+++ Codeforces/Div 2/Codeforces Round 996 (Div. 2)/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define DEBUG 1
+#define OUT(x) cerr<<(#x)<<'='<<(x)<<endl
+
+void solve(){
+    int n, a, b; cin >> n >> a >> b;
+    cout << (abs(b - a) % 2 == 1 ? "NO\n" : "YES\n");
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tt; cin >> tt;
+    while(tt--){
+        solve();
+    }
+}

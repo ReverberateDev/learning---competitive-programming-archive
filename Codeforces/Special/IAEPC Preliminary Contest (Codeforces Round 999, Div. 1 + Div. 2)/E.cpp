15947b6200519919651bc0071c3e220cca27d24d
Raphael Lim Fu En <raphaellim.dev@gmail.com>
2025-02-07 15:58:30 +0000

    added E.cpp div 2

--- Codeforces/Special/IAEPC Preliminary Contest (Codeforces Round 999, Div. 1 + Div. 2)/E.cpp
+++ Codeforces/Special/IAEPC Preliminary Contest (Codeforces Round 999, Div. 1 + Div. 2)/E.cpp
@@ -6,26 +6,7 @@ using namespace std;
 #define OUT(x) cerr<<(#x)<<'='<<(x)<<endl
 
 const int inf = 1e18;
-/*
-struct info{
-    int lost, ptr, idx;
-    info(int _idx, vector<vector<int>> &best): idx(_idx), ptr(0){
-        lost = best[idx][ptr] - best[idx][ptr + 1];
-    }
-    void nxt(vector<vector<int>> &best){
-        if(ptr < best[idx].size() - 1){
-            ptr++;
-            lost = best[idx][ptr] - best[idx][ptr + 1];
-        }
-        else{
-            lost = inf;
-        }
-    }
-    bool operator<(const info &other) const {
-        return lost < other.lost;
-    }
-};
-*/
+
 void solve(){
     int n, m, k; cin >> n >> m >> k;
     vector<int> a(n), b(m);
@@ -35,46 +16,42 @@ void solve(){
     for(int i = 0; i < m; i++){
         cin >> b[i];
     }
-    vector<vector<int>> best(n, vector<int>(m + 2, inf));
-    for(int i = 0; i < n; i++){
-        best[i][0] = a[i];
-        for(int bmask = 0; bmask < (1 << m); bmask++){
-            int j = __builtin_popcountll(bmask);
-            int curr = best[i][0];
-            for(int idx = 0; idx < m; idx++){
-                if(bmask & (1 << idx)){
-                    curr &= b[idx];
-                }
+    vector<int> no(1 << m), bc(1 << m); 
+    for(int bmask = 0; bmask < (1 << m); bmask++){
+        int curr = (1 << 30) - 1;
+        for(int i = 0; i < m; i++){
+            if(bmask & (1 << i)){
+                curr &= b[i];
             }
-            best[i][j] = min(best[i][j], curr);
         }
+        no[bmask] = curr;
+        bc[bmask] = __builtin_popcountll(curr);
     }
-    if(DEBUG){
-        cout << "Outputting best\n";
-        for(auto &a : best){
-            for(auto &b : a){
-                cout << b << " ";
-            }
-            cout << '\n';
-        }
-    }
+    
+    vector<int> gains;
     int ans = 0;
-    vector<int> vect;
     for(int i = 0; i < n; i++){
-        ans += best[i][0];
-        for(int j = 0; j < m + 1; j++){
-            vect.push_back(best[i][j] - best[i][j + 1]);
+        vector<int> best(m + 1, 1e18);
+        best[0] = a[i];
+        ans += a[i];
+        for(int bmask = 0; bmask < (1 << m); bmask++){
+            best[bc[bmask]] = min(best[bc[bmask]], a[i] & no[bmask]);
+        }
+        for(int j = 0; j <= m - 1; j++){
+            //cout << "hello\n";
+            gains.push_back(1);
         }
     }
-    sort(vect.begin(), vect.end());
-    reverse(vect.begin(), vect.end());
-    for(int i = 0; i < k; i++){
-        if(vect[i] <= 0){
+    /*
+    int cnt = 0;
+    while(k--){
+        if(gains[cnt] <= 0){
             break;
         }
-        ans -= vect[i];
+        ans -= gains[cnt++];
     }
     cout << ans << '\n';
+    */
 }
 
 signed main(){
